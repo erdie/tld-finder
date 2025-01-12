a85b4b0a80ed9dd80404b7055b74089ec9657834
Erdiawan Anna <erdie.one@gmail.com>
2025-01-12 22:56:19 +0700

    fix(c/theme-provider): render hydration

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,6 +5,15 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+    const [mounted, setMounted] = React.useState(false)
+
+    React.useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return <>{children}</>
+    }
+
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
