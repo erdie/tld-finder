225233b10fe4ec3840faba6f2825b952497a3364
Erdiawan Anna <erdie.one@gmail.com>
2026-08-05 16:07:57 +0700

    feat: simplify ThemeProvider by removing mounted state handling

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,15 +5,5 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    const [mounted, setMounted] = React.useState(false)
-
-    React.useEffect(() => {
-        setMounted(true)
-    }, [])
-
-    if (!mounted) {
-        return <>{children}</>
-    }
-
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
