2422aa2becbda6d42137017817fb93829b71ae2a
Erdiawan Anna <erdie.one@gmail.com>
2026-08-17 19:29:37 +0700

    feat: remove unused font preconnect and stylesheet links from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,12 +105,6 @@ export default function RootLayout({
     return (
         <html lang="en" className="dark" suppressHydrationWarning>
             <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Material+Symbols-Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
-                />
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
